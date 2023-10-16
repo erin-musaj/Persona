5960fbee7ca83eb110a1f8a69932977950427ff0
erin-musaj <erin.musaj@buonarroti.tn.it>
2023-10-16 08:51:41 +0200

    feat: constructor Calciatore

--- src/Calciatore.java
+++ src/Calciatore.java
@@ -1,2 +1,15 @@
-public class Calciatore {
+public class Calciatore extends Persona {
+    private String ruolo;
+    private int numeroMaglia;
+    private int numeroGoal;
+
+    public Calciatore(
+            String nome, String sesso, int anno, int mese, int giorno,
+            String ruolo, int numeroMaglia, int numeroGoal
+    ) throws Exception {
+        super(nome, sesso, anno, mese, giorno);
+        this.ruolo=ruolo;
+        this.numeroGoal=numeroGoal;
+        this.numeroMaglia=numeroMaglia;
+    }
 }
