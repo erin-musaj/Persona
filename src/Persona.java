b44ac2dfc6f9f78839487ae2405210a127819ed6
erin-musaj <erin.musaj@buonarroti.tn.it>
2023-10-16 08:24:57 +0200

    feat: metodi base

--- src/Persona.java
+++ src/Persona.java
@@ -1,2 +1,18 @@
+import java.util.Date;
+
 public class Persona {
+
+    private String nome;
+    private String sesso;
+    private Date compleanno;
+
+    public Persona(String nome, String sesso, Date compleanno) {
+        this.nome = nome;
+        this.sesso = sesso;
+        this.compleanno = compleanno;
+    }
+
+    public String toString(){
+        return "{nome:" + this.nome + ",sesso:" + this.sesso + ",compleanno:" + this.compleanno + "}";
+    }
 }
